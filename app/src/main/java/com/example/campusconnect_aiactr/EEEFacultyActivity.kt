45977b3e465870c7c15efcc6f550a18ba8cefa51
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2023-04-07 16:11:32 +0530

    Campus connect-AIACTR commit done

---
+++ app/src/main/java/com/example/campusconnect_aiactr/EEEFacultyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.campusconnect_aiactr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EEEFacultyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eeefaculty)
+    }
+}
