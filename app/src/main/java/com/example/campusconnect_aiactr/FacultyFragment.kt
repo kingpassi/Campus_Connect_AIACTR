08816e4eb8a98421cf43237723f1afe33baf6e44
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2023-04-07 16:15:03 +0530

    Campus connect-AIACTR commit done

--- app/src/main/java/com/example/campusconnect_aiactr/FacultyFragment.kt
+++ app/src/main/java/com/example/campusconnect_aiactr/FacultyFragment.kt
@@ -1,59 +1,69 @@
 package com.example.campusconnect_aiactr
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FacultyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FacultyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_faculty, container, false)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FacultyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FacultyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val director=view.findViewById<CardView>(R.id.DirectorCardView)
+        val ECEFaculty=view.findViewById<CardView>(R.id.ECEFacultyCardView)
+        val CSEFaculty=view.findViewById<CardView>(R.id.CSEFacultyCardView)
+        val SAHFaculty=view.findViewById<CardView>(R.id.SAHFacultyCardView)
+        val EEEFaculty=view.findViewById<CardView>(R.id.EEEFacultyCardView)
+
+        director.setOnClickListener {
+            val directorIntroIntent= Intent(context,DirectorIntroActivity::class.java)
+            startActivity(directorIntroIntent)
+
+        }
+
+        ECEFaculty.setOnClickListener {
+            val ECEFacultyIntent=Intent(context,ECEFacultyActivity::class.java)
+            startActivity(ECEFacultyIntent)
+        }
+
+        CSEFaculty.setOnClickListener {
+            val CSEFAcultyIntent=Intent(context,CSEFacultyActivity::class.java)
+            startActivity(CSEFAcultyIntent)
+        }
+
+        SAHFaculty.setOnClickListener {
+            val SAHFacultyIntent=Intent(context,SAHFacultyActivity::class.java)
+            startActivity(SAHFacultyIntent)
+        }
+        EEEFaculty.setOnClickListener {
+            val EEEFacultyIntent=Intent(context,EEEFacultyActivity::class.java)
+            startActivity(EEEFacultyIntent)
+        }
+
+
     }
+
+
+
+
 }
+
+
+
--- app/src/main/java/com/example/campusconnect_aiactr/StudentFragment.kt
+++ app/src/main/java/com/example/campusconnect_aiactr/StudentFragment.kt
@@ -1,33 +1,22 @@
 package com.example.campusconnect_aiactr
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class StudentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class StudentFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +26,27 @@ class StudentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_student, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val BTechStudent=view.findViewById<CardView>(R.id.BTechStudentCardView)
+        val MtechStudent=view.findViewById<CardView>(R.id.MTechStudentCardView)
+        val StudentCommunity=view.findViewById<CardView>(R.id.StudentCommunityCardView)
+
+
+        BTechStudent.setOnClickListener {
+            val BTechStudentIntent=Intent(context,BTechStudentActivity::class.java)
+            startActivity(BTechStudentIntent)
+        }
+        MtechStudent.setOnClickListener {
+            val MtechStudentIntent=Intent(context,MTechStudentActivity::class.java)
+            startActivity(MtechStudentIntent)
+        }
+
+        StudentCommunity.setOnClickListener {
+            val StudentCommunityIntent=Intent(context,StudentCommunityActivity::class.java)
+            startActivity(StudentCommunityIntent)
+        }
+
     }
 }
